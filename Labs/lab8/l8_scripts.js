12442d3e0da9824742c21d107e033db62850b641
shkelliton <shkelliton@users.noreply.github.com>
2020-11-08 20:56:59 -0500

    Lab 8 work +some assignment
    Finished lab 8, incomplete assignment work

---
+++ Labs/lab8/l8_scripts.js
@@ -0,0 +1,17 @@
+function pushMe() {
+  document.getElementById("output").innerHTML = "Hurrah!";
+}
+
+function mix() {
+  document.getElementById("blue").style.color = "blue";
+  document.getElementById("green").style.color = "green";
+  document.getElementById("red").style.color = "red";
+}
+
+function theRest() {
+  document.getElementById("output0").innerHTML = "Zero";
+  document.getElementById("output1").innerHTML = "One";
+  document.getElementById("output2").innerHTML = "Two";
+  document.getElementById("output3").innerHTML = "Three";
+
+}

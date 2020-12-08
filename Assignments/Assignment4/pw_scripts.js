0d4a21be0d4f231c739055a410ac6920dd98d6ca
shkelliton <71197165+shkelliton@users.noreply.github.com>
2020-12-07 20:27:24 -0500

    Website design and functionality
    Almost there, will finish doc at home

--- Assignments/Assignment4/pw_scripts.js
+++ Assignments/Assignment4/pw_scripts.js
@@ -1,84 +1,24 @@
-//NOTES + TO DO
-// find height of wrapper, set image height same as wrapper
-// scale for content
-// make content height wrapperheight- headerheight
-// figure out better name title color/method
-// make home page 'About' page
-// gallery function for artwork pages - multiple separate galleries for Sequential
-// and character design?
-// for pricing page, refer to image saved
-// format like java with methods and that garbage
-
-//IF POSSIBLE/IF TIME ALLOWS
-//See if able to make website actually send an email for inquiries?
-//see if you can change format based on device, is there a detect mobile function?
-//maybe logo instead of actual name, put name in content area in its own div or something
-//If I'm feeling adventurous, make a little animation around logo
-//remove other navbar options on other pages 
-
-//ACTUAL CODE, NOT NOTES
 //GLOBAL VARIABLES
-
-var element = $("#whatever Div I choose"); //element - make function to find element instead of single variable
-var elHeight = element.outerHeight(); //finds height of specific element
-var elWidth = element.outerWidth(); //finds width of specific element
-
+var start = new Date();
 //COMMAND AREA
-
-
-
-
-
-
-
-
-
 
 //FUNCTIONS
 
+function stopTime(){
 
-
-
-
-
-
-
-
-
-
-
-
-
-//call stuff using <script>?
-var $el = $("#very-specific-design"); //makes variable this element
-var elHeight = $el.outerHeight(); //finds height of specific element
-var elWidth = $el.outerWidth(); //finds width of specific element
-
-var $wrapper = $("#scaleable-wrapper");
-
-$wrapper.resizable({ //the little arrow on the bottom right to change element size with mouse
-  resize: doResize
-});
-
-function doResize(event, ui) {
-
-  var scale, origin;
-
-  scale = Math.min( //math formula to recalculate size
-    ui.size.width / elWidth,
-    ui.size.height / elHeight
-  );
-
-  $el.css({ //no idea what this does
-    transform: "translate(-50%, -50%) " + "scale(" + scale + ")"
-  });
-
+  var stop = new Date();
+  console.log("newstart"+start.getSeconds())
+  console.log(stop.getSeconds())
+  var timeDif = stop.getTime()-start.getTime()
+  alert("You have been on this website for:"+ (timeDif/600)+"seconds");
 }
 
-var starterData = {
-  size: {
-    width: $wrapper.width(),
-    height: $wrapper.height()
-  }
+function doZoom(imgs){
+  var bigIMG = document.getElementById('bigIMG');
+  bigIMG.src = imgs.src;
+  bigIMG.parentElement.style.display = "block";
 }
-doResize(null, starterData);
+function infoGather(imgs){
+  var infoTransfer = document.getElementById("imgDescrip");
+  infoTransfer.innerHTML = imgs.alt;
+}
